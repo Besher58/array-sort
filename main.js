71a06524dfcd716c76109cbb480ffd42e306c208
besher58 <beshr58@gmail.com>
2020-07-16 16:07:43 +0200

    array-sort

---
+++ main.js
@@ -0,0 +1,16 @@
+var languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+  ];
+let test=languages.sort()  
+console.log(test)
